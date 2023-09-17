fde8e2a68d86d4daee4086d441f8713f27a90c80
Mark Urquhart <mwurquha@gmail.com>
2023-09-16 23:04:18 -0400

    refactored server to use async and explicit serverAPI vers

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const PORT = 3000;
 
 const uri = process.env.MONGO_CONNECT_STR;
-
-let db;
 
 // Middleware for parsing JSON requests
 app.use(express.json());
@@ -16,47 +14,60 @@ app.use(express.json());
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-MongoClient.connect(uri, function(err, client) {
-    if (err) {
+let db;
+
+async function initializeServer() {
+    try {
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+
+        await client.connect();
+        console.log("Connected successfully to MongoDB");
+
+        db = client.db("library");
+
+        app.listen(PORT, () => {
+            console.log(`Server started on http://localhost:${PORT}`);
+        });
+
+        app.get('/', (req, res) => {
+            res.send('Hello, MongoDB Library!');
+        });
+
+        app.get('/books', (req, res) => {
+            db.collection('books').find().toArray((err, books) => {
+                if (err) {
+                    res.status(500).send('Error fetching books.');
+                    return;
+                }
+                res.json(books);
+            });
+        });
+
+        app.post('/books', (req, res) => {
+            const newBook = {
+                title: req.body.title,
+                author: req.body.author,
+            };
+
+            db.collection('books').insertOne(newBook, (err, result) => {
+                if (err) {
+                    res.status(500).send('Failed to add book.');
+                    return;
+                }
+                res.status(201).send(result.ops[0]);
+            });
+        });
+
+    } catch (err) {
         console.error('Failed to connect to the database.', err);
         process.exit(1);
     }
+}
 
-    console.log("Connected successfully to MongoDB");
-    db = client.db("library");
-
-    app.listen(PORT, () => {
-        console.log(`Server started on http://localhost:${PORT}`);
-    });
-});
-
-// This route may not be necessary since we're serving index.html from the public directory now
-// If you still want to send a string response on hitting the root, keep it.
-app.get('/', (req, res) => {
-    res.send('Hello, MongoDB Library!');
-});
-
-app.get('/books', (req, res) => {
-    db.collection('books').find().toArray((err, books) => {
-        if (err) {
-            res.status(500).send('Error fetching books.');
-            return;
-        }
-        res.json(books);
-    });
-});
-
-app.post('/books', (req, res) => {
-    const newBook = {
-        title: req.body.title,
-        author: req.body.author,
-    };
-
-    db.collection('books').insertOne(newBook, (err, result) => {
-        if (err) {
-            res.status(500).send('Failed to add book.');
-            return;
-        }
-        res.status(201).send(result.ops[0]);  
-    });
-});
+initializeServer().catch(console.error);
