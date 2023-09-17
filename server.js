6f7060a17231c019ae8e5cdb6280c790c558897d
Mark Urquhart <mwurquha@gmail.com>
2023-09-16 22:31:17 -0400

    Edit mongo connection sequence and add .env file

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, MongoDB Library!');
+const uri = process.env.MONGO_CONNECT_STR;
+console.log(uri)
+let db;
+
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+    if (err) {
+        console.error('Failed to connect to the database.', err);
+        process.exit(1);
+    }
+
+    console.log("Connected successfully to MongoDB");
+    db = client.db("library");
+
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`);
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+    res.send('Hello, MongoDB Library!');
 });
